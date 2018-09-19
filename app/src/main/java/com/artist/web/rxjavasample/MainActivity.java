34264d27dc4170a6716610e2b0ce86fb16520213
WEBARTIST\User <hennasingh04@gmail.com>
2018-09-19 19:05:45 +0100

    added observables and observers

--- app/src/main/java/com/artist/web/rxjavasample/MainActivity.java
+++ app/src/main/java/com/artist/web/rxjavasample/MainActivity.java
@@ -2,17 +2,63 @@ package com.artist.web.rxjavasample;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 
 import io.reactivex.Observable;
+import io.reactivex.Observer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.schedulers.Schedulers;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Observable<String> animalsObservable = Observable.just("Ant", "Bee", "Cat", "Dog", "Fox");
+        //Observable
+        Observable<String> animalsObservable = getAnimalsObservable();
 
+        //Observer
+        Observer<String> animalsObserver = getAnimalObserver();
+
+        //observer subscribing to observable
+        animalsObservable
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(animalsObserver);
+
+    }
+
+    private Observable<String> getAnimalsObservable() {
+        return Observable.just("Ant", "Bee", "Cat", "Dog", "Fox");
+    }
+
+    private Observer<String> getAnimalObserver() {
+        return new Observer<String>() {
+            @Override
+            public void onSubscribe(Disposable d) {
+                Log.d(TAG, "onSubscribe");
+            }
+
+            @Override
+            public void onNext(String s) {
+                Log.d(TAG, "Name: " + s);
+            }
+
+            @Override
+            public void onError(Throwable e) {
+                Log.e(TAG, "onError: " + e.getMessage());
+            }
+
+            @Override
+            public void onComplete() {
+                Log.d(TAG, "All items are emitted!");
+            }
+        };
     }
 }
